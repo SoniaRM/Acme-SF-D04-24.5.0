928789e9ea1f2baa638099efd435d447bbf18682
Ruben Perez <rupg29@gmail.com>
2024-05-16 13:58:44 +0200

    [fix]: fixed variable names in dashboards

--- src/main/java/acme/features/client/dashboard/ClientDashboardRepository.java
+++ src/main/java/acme/features/client/dashboard/ClientDashboardRepository.java
@@ -10,28 +10,28 @@ import acme.roles.Client;
 @Repository
 public interface ClientDashboardRepository extends AbstractRepository {
 
-	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and pl.completeness < 25 and pl.contract.draftMode = false")
+	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and pl.completeness < 0.25")
 	int findTotalLogLessThan25(Client client);
 
-	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and pl.completeness < 50 and 25 <= pl.completeness and pl.contract.draftMode = false")
+	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and pl.completeness < 0.50 and 0.25 <= pl.completeness")
 	int findTotalLogLessBetween25And50(Client client);
 
-	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and pl.completeness < 75 and 50 <= pl.completeness and pl.contract.draftMode = false")
+	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and pl.completeness < 0.75 and 0.50 <= pl.completeness")
 	int findTotalLogLessBetween50And75(Client client);
 
-	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and 75 <= pl.completeness and pl.contract.draftMode = false")
+	@Query("select count(pl) from ProgressLog pl where pl.contract.client = :client and 0.75 <= pl.completeness")
 	int findTotalLogAbove75(Client client);
 
-	@Query("select avg(c.budget.amount) from Contract c where c.client = :client and c.draftMode = false")
+	@Query("select avg(c.budget.amount) from Contract c where c.client = :client")
 	Double findAverageBudgetContracts(Client client);
 
-	@Query("select stddev(c.budget.amount) from Contract c where c.client = :client and c.draftMode = false")
+	@Query("select stddev(c.budget.amount) from Contract c where c.client = :client")
 	Double findDeviationBudgetContracts(Client client);
 
-	@Query("select min(c.budget.amount) from Contract c where c.client = :client and c.draftMode = false")
+	@Query("select min(c.budget.amount) from Contract c where c.client = :client")
 	Double findMinimunBudgetContracts(Client client);
 
-	@Query("select max(c.budget.amount) from Contract c where c.client = :client and c.draftMode = false")
+	@Query("select max(c.budget.amount) from Contract c where c.client = :client")
 	Double findMaximumBudgetContracts(Client client);
 
 	@Query("select c from Client c where c.userAccount.id = :id")
